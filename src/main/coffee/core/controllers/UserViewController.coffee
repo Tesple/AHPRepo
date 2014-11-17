9fd4a4de4d07023b8b3936fbdbc3014aa9edaab9
WebNoyer <tomasz.piechaczek@bitbar.com>
2014-11-17 19:04:19 +0100

    Controlers and styles update

---
+++ src/main/coffee/core/controllers/UserViewController.coffee
@@ -0,0 +1,6 @@
+class app.core.controllers.UserViewController
+
+  constructor: ->
+    super()
+    _info("UserViewController initialized")
+    @
--- src/main/js/Utils.js
+++ src/main/js/Utils.js
@@ -1,3 +1,7 @@
 function _capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function _info(string) {
+    console.info(string);
+}
